506318b7860aa1862329ecc36693424d4f24e151
CharithaDhananjaya <charitha.dhananjaya464@gmail.com>
2020-04-03 10:47:48 +0530

    Users and Role Table configured with Sequealize.

---
+++ models/role.model.js
@@ -0,0 +1,13 @@
+//Sequelize Model for the Role Table
+module.exports = (sequelize, Sequelize) => {
+    const Role = sequelize.define("roles", {
+        id: {
+            type: Sequelize.INTEGER,
+            primaryKey: true
+        },
+        name: {
+            type: Sequelize.STRING
+        }
+    });
+    return Role;
+}
