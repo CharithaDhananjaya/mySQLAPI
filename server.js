3d61c75eebe05df2d6bab6f0564f1051f4b36888
CharithaDhananjaya <charitha.dhananjaya464@gmail.com>
2020-04-03 15:09:05 +0530

    Changed to master. Just checking

--- server.js
+++ server.js
@@ -5,5 +5,5 @@ const port = process.env.PORT || 8000;
 
 http.createServer(app)
     .listen(port, () => {
-        console.log(`Your Server is Running on Port ${port}`);
+        console.log(`Your Server is up and Running on Port ${port}`);
     });
