d108d4e39f2030095d88211c955a9d4d6f53ad01
CharithaDhananjaya <charitha.dhananjaya464@gmail.com>
2020-04-03 10:47:23 +0530

    Auth model added.

--- config/db.config.js
+++ config/db.config.js
@@ -2,7 +2,14 @@ const db = {
     HOST: "localhost",
     USER: "root",
     PASSWORD: "password",
-    DATABASE: "mydb"
+    DATABASE: "mydb",
+    dialect: "mysql",
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
 }
 
 module.exports = db;
